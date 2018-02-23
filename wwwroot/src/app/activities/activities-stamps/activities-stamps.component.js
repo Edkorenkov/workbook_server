7f6fb6479ca681806f1fdb08f7518c2c00a5bff6
Eduard Korenkov <eduard.korenkov@playtika.com>
2018-02-23 17:53:52 +0200

    Refactor activities.

---
+++ wwwroot/src/app/activities/activities-stamps/activities-stamps.component.js
@@ -0,0 +1,20 @@
+
+import { Component } from "@angular/core";
+
+
+@Component({
+
+    selector: "activities-stamps",
+
+    templateUrl: "./activities-stamps.component.html",
+    
+    styleUrls: [],
+
+})
+export class ActivitiesStampsComponent {
+
+    constructor() {
+
+    };
+
+};
